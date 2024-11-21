4dba085211336043b750a6496cc4f6f536e6a46f
llucasmenezes <llucasmenezes99@gmail.com>
2024-11-21 12:09:01 -0300

    feat(db): adicionar duas colunas na tabela [tb_mae] usando migração Flyway

---
+++ src/main/resources/db/migration/V3__add_column_email.sql
@@ -0,0 +1,5 @@
+--V3 join column email in table tb_mae
+
+ALTER TABLE tb_mae
+ADD COLUMN email VARCHAR(255);
+
---
+++ src/main/resources/db/migration/V4__add_column_celular.sql
@@ -0,0 +1,4 @@
+--V4 join column celular
+
+ALTER TABLE tb_mae
+ADD COLUMN celular VARCHAR(16);
