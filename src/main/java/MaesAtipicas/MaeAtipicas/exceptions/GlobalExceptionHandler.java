b5b03d91b544db5f06f15fa893c15e139069ef97
llucasmenezes <llucasmenezes99@gmail.com>
2024-11-21 17:13:31 -0300

    feat: adicionando excecoes globais, desacoplando responsabilidades dos controllers.

--- src/main/java/MaesAtipicas/MaeAtipicas/exceptions/GlobalExceptionHandler.java
+++ src/main/java/MaesAtipicas/MaeAtipicas/exceptions/GlobalExceptionHandler.java
@@ -21,6 +21,7 @@ public class GlobalExceptionHandler {
         return  new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
+    @ExceptionHandler(NoId.class)
     public ResponseEntity<ErrorResponse> handleNoId(NoId e){
         ErrorResponse errorResponse = new ErrorResponse("Nenhum usuario", e.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
