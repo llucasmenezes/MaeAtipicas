063fda00d6231d867360c334b997dd5ecbbc1e0b
llucasmenezes <llucasmenezes99@gmail.com>
2024-11-26 12:23:25 -0300

    chore: refatorando o mapper, para a funcionalidade do mapStruct, otimizando a aplicacao.

--- src/main/java/MaesAtipicas/MaeAtipicas/mapper/AddressMapper.java
+++ src/main/java/MaesAtipicas/MaeAtipicas/mapper/AddressMapper.java
@@ -2,33 +2,13 @@ package MaesAtipicas.MaeAtipicas.mapper;
 
 import MaesAtipicas.MaeAtipicas.DTO.AddressDTO;
 import MaesAtipicas.MaeAtipicas.model.AddressModel;
+import org.mapstruct.Mapper;
 import org.springframework.stereotype.Component;
 
-@Component
-public class AddressMapper {
+@Mapper(componentModel = "spring")
+public interface AddressMapper {
 
-    public AddressModel map(AddressDTO addressDTO){
-        AddressModel addressModel = new AddressModel();
-        addressModel.setId(addressDTO.getId());
-        addressModel.setCountry(addressDTO.getCountry());
-        addressModel.setCity(addressDTO.getCity());
-        addressModel.setPostalcode(addressDTO.getPostalcode());
-        addressModel.setStreet(addressDTO.getStreet());
-        addressModel.setMaeModel(addressDTO.getMaeModel());
+    AddressModel map(AddressDTO addressDTO);
+    AddressDTO map(AddressModel addressModel);
 
-        return addressModel;
-    }
-
-    public AddressDTO map(AddressModel addressModel){
-        AddressDTO addressDTO = new AddressDTO();
-        addressDTO.setId(addressModel.getId());
-        addressDTO.setCountry(addressModel.getCountry());
-        addressDTO.setState(addressModel.getState());
-        addressDTO.setCity(addressModel.getCity());
-        addressDTO.setPostalcode(addressModel.getPostalcode());
-        addressDTO.setStreet(addressModel.getStreet());
-        addressDTO.setMaeModel(addressModel.getMaeModel());
-
-        return addressDTO;
-    }
 }
--- src/main/java/MaesAtipicas/MaeAtipicas/mapper/MaeMapper.java
+++ src/main/java/MaesAtipicas/MaeAtipicas/mapper/MaeMapper.java
@@ -2,29 +2,13 @@ package MaesAtipicas.MaeAtipicas.mapper;
 
 import MaesAtipicas.MaeAtipicas.DTO.MaeDTO;
 import MaesAtipicas.MaeAtipicas.model.MaeModel;
+import org.mapstruct.Mapper;
 import org.springframework.stereotype.Component;
 
 
-@Component
-public class MaeMapper {
+@Mapper(componentModel = "spring")
+public interface MaeMapper {
 
-    public MaeModel map(MaeDTO maeDTO) {
-        MaeModel maeModel = new MaeModel();
-        maeModel.setId(maeDTO.getId());
-        maeModel.setName(maeDTO.getName());
-        maeModel.setLastName(maeDTO.getLastName());
-        maeModel.setCpf(maeDTO.getCpf());
-        maeModel.setAdressModel(maeDTO.getAddressModel());
-        return maeModel;
-    }
-
-    public MaeDTO map(MaeModel maeModel){
-        MaeDTO maeDTO = new MaeDTO();
-        maeDTO.setId(maeModel.getId());
-        maeDTO.setName(maeModel.getName());
-        maeDTO.setLastName(maeModel.getLastName());
-        maeDTO.setCpf(maeModel.getCpf());
-        maeDTO.setAddressModel(maeDTO.getAddressModel());
-        return maeDTO;
-    }
+   MaeModel map(MaeDTO maeModel);
+   MaeDTO map(MaeModel maeModel);
 }
