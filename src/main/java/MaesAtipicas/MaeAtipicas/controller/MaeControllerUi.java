8eb8b34a9c350ee276ce2ede9dacb9af1945750e
llucasmenezes <llucasmenezes99@gmail.com>
2024-11-27 20:14:45 -0300

    feat: criando o controller UI, para a entrega visual dos dados

--- src/main/java/MaesAtipicas/MaeAtipicas/controller/MaeControllerUi.java
+++ src/main/java/MaesAtipicas/MaeAtipicas/controller/MaeControllerUi.java
@@ -1,4 +1,35 @@
 package MaesAtipicas.MaeAtipicas.controller;
 
-public class MaeControllerUI {
+import MaesAtipicas.MaeAtipicas.DTO.MaeDTO;
+import MaesAtipicas.MaeAtipicas.service.MaeService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("maes/ui")
+public class MaeControllerUi {
+
+    private final MaeService maeService;
+
+    public MaeControllerUi(MaeService maeService){
+        this.maeService = maeService;
+    }
+
+
+    @GetMapping("/listar")
+    public String getAll(Model model) {
+        List<MaeDTO> maes = maeService.getAll();
+        model.addAttribute("maes", maes);
+        return "listarMaes";
+    }
+
+
+
+
 }
