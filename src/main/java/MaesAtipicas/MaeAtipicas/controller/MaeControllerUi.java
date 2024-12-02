0f4e0ee66aa8d19c5fe0bd313a9449f80c2f2a02
llucasmenezes <llucasmenezes99@gmail.com>
2024-12-02 19:45:14 -0300

    feat: incluindo no html junto com uma estilizacao, de listar maes, adicionar mae e ver detalhes da mae. e consequentemente incluindo no MaeControllerUi.

--- src/main/java/MaesAtipicas/MaeAtipicas/controller/MaeControllerUi.java
+++ src/main/java/MaesAtipicas/MaeAtipicas/controller/MaeControllerUi.java
@@ -1,18 +1,17 @@
 package MaesAtipicas.MaeAtipicas.controller;
-
 import MaesAtipicas.MaeAtipicas.DTO.MaeDTO;
+import MaesAtipicas.MaeAtipicas.model.MaeModel;
 import MaesAtipicas.MaeAtipicas.service.MaeService;
-import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
-@RequestMapping("maes/ui")
+@RequestMapping("mae/ui")
 public class MaeControllerUi {
 
     private final MaeService maeService;
@@ -27,6 +26,48 @@ public class MaeControllerUi {
         List<MaeDTO> maes = maeService.getAll();
         model.addAttribute("maes", maes);
         return "listarMaes";
+    }
+
+    @GetMapping("/deletar/{id}")
+    public String deleteMaeById(@PathVariable Long id) {
+        maeService.deleteMae(id);
+        return "redirect:/mae/ui/listar";
+    }
+
+    @GetMapping("/listar/{id}")
+    public String getMaeById(@PathVariable Long id, Model model) {
+        Optional<MaeDTO> mae = maeService.getMaeById(id);
+
+        if (!mae.isPresent()) {
+            model.addAttribute("mensagem", "QUE PENA :(... Mãe não encontrada");
+            return "listarMaes";
+        }
+
+        MaeDTO maeDTO = mae.get();
+
+        if (maeDTO.getAddressModel() != null) {
+            model.addAttribute("endereco", maeDTO.getAddressModel());
+        } else {
+            model.addAttribute("mensagemEndereco", "Endereço não disponível");
+        }
+
+        model.addAttribute("mae", maeDTO);
+        return "detalhesMae";
+    }
+
+
+
+    @GetMapping("/adicionar")
+    public String mostrarFormularioAdicionarNinja(Model model) {
+        model.addAttribute("mae", new MaeDTO());
+        return "adicionarMae";
+    }
+
+    @PostMapping("/salvar")
+    public String createMae(@RequestBody MaeDTO mae, RedirectAttributes redirectAttributes) {
+        maeService.createMae(mae);
+        redirectAttributes.addFlashAttribute("mensagem", "EBAA MAEE! seu cadastro foi concluido com sucesso. :)" );
+        return "redirect:mae/ui/listar";
     }
 
 
